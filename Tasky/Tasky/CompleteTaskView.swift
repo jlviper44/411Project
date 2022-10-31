645d7655b0b114482c8bd9b936d64b89e4186961
Justin Lee <jlviper44@csu.fullerton.edu>
2022-10-30 22:00:03 -0700

    Added Initial Views

--- Tasky/Tasky/CompleteTaskView.swift
+++ Tasky/Tasky/CompleteTaskView.swift
@@ -7,7 +7,7 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class CompleteTaskView: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
---
+++ Tasky/Tasky/IncompleteTaskView.swift
@@ -0,0 +1,20 @@
+//
+//  UncompleteTaskView.swift
+//  Tasky
+//
+//  Created by Justin Lee on 10/30/22.
+//
+
+import UIKit
+
+class IncompleteTaskView: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+    
+    
+
+
+}
